9b0f95488f83c321f753ebfda1ab69551ad52356
unknown <zlovred12@gmail.com>
2022-04-30 17:28:08 +0300

    temp

--- src/main/java/ru/imbabot/java3/lesson7/TestExecute.java
+++ src/main/java/ru/imbabot/java3/lesson7/TestExecute.java
@@ -8,13 +8,21 @@ public class TestExecute {
 
     public static void start(Class c) {
         Method[] methods = c.getDeclaredMethods();
-        int temp = 2;
-        while (temp <= 9){
+        int temp = 1;
+        while (temp <= 10){
             for (int i = 0; i <methods.length ; i++) {
-              if (methods[i].isAnnotationPresent(Test.class) && methods[i].getAnnotation(Test.class).priority() == temp){
-                  System.out.println(methods[i].getName());
-                  temp++;
-              }
+                if (methods[i].isAnnotationPresent(Test.class) && methods[i].getAnnotation(Test.class).priority() == temp){
+                    System.out.println(methods[i].getName());
+                    temp++;
+                }
+                if (methods[i].isAnnotationPresent(BeforeSuite.class) && methods[i].getAnnotation(BeforeSuite.class).priority() == temp){
+                    System.out.println(methods[i].getName());
+                    temp++;
+                }
+                if (methods[i].isAnnotationPresent(AfterSuite.class) && methods[i].getAnnotation(AfterSuite.class).priority() == temp){
+                    System.out.println(methods[i].getName());
+                    temp++;
+                }
             }
         }
 
@@ -27,6 +35,6 @@ public class TestExecute {
 
 
     public static void main(String[] args) {
-            start(Tests.class);
+        start(Tests.class);
     }
 }
--- src/main/java/ru/imbabot/java3/lesson7/Tests.java
+++ src/main/java/ru/imbabot/java3/lesson7/Tests.java
@@ -11,31 +11,31 @@ public class Tests {
         System.out.println("test1");
     }
     @Test(priority = 3)
-    public void test3(){
+    public void test2(){
         System.out.println("test2");
     }
     @Test(priority = 4)
-    public void test4(){
+    public void test3(){
         System.out.println("test3");
     }
     @Test(priority = 5)
-    public void test5(){
+    public void test4(){
         System.out.println("test4");
     }
     @Test(priority = 6)
-    public void test6(){
+    public void test5(){
         System.out.println("test5");
     }
     @Test(priority = 7)
-    public void test7(){
+    public void test6(){
         System.out.println("test6");
     }
     @Test(priority = 8)
-    public void test8(){
+    public void test7(){
         System.out.println("test7");
     }
     @Test(priority = 9)
-    public void test9(){
+    public void test8(){
         System.out.println("test8");
     }
     @AfterSuite(priority = 10)
