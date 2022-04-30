7f5a20010d924a9daa3cc9257cd471d4a239de4e
unknown <zlovred12@gmail.com>
2022-04-30 14:37:05 +0300

    temp

--- src/main/java/ru/imbabot/java3/lesson7/TestExecute.java
+++ src/main/java/ru/imbabot/java3/lesson7/TestExecute.java
@@ -1,16 +1,22 @@
 package ru.imbabot.java3.lesson7;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class TestExecute {
 
-    public static void start(Class c){
+    public static void start(Class c) {
         Method[] methods = c.getDeclaredMethods();
-        for (Method m: methods){
-            System.out.println(m.getName());
+        int[]arr = new int[methods.length];
+        for (int i = 0; i < methods.length ; i++) {
+            for (int j = 0; j < methods.length ; j++) {
+                if (methods[i].isAnnotationPresent(Test.class)){
+                    arr[i] = methods[i].getAnnotation(Test.class).priority();
+                }
+            }
         }
-
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void main(String[] args) {
